0d1fab73ddd415710c052655dcc8272ba2d741c3
WederTito <wedertito8@gmail.com>
2022-08-07 02:36:02 -0300

    Cadastro de categoria

--- livro/urls.py
+++ livro/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path ('home/' , views.home, name = 'home'),
     path ('ver_livros/<int:id>', views.ver_livros, name ='ver_livros'),
-    path ('cadastar_livro', views.cadastar_livro, name='cadastar_livro')
+    path ('cadastar_livro', views.cadastar_livro, name='cadastar_livro'),
+    path ('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro'),
+    path ('cadastar_categoria/', views.cadastar_categoria, name='cadastar_categoria')
 ]
